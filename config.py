211e6d8e49e8163e68ec5885b65082bacf830865
NikitaMulyar <nickm.school@mail.ru>
2023-10-19 00:34:32 +0300

    .

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = '6600430355:AAG4SAnxa3t5QHf-swRuIR7ypobG8JKs6wg'
-# BOT_TOKEN = '6306869964:AAHOBsBqXnnLAoCn-edg47CuncGyJ0kqeLs'  # бот для теста администрации
+# BOT_TOKEN = '6600430355:AAG4SAnxa3t5QHf-swRuIR7ypobG8JKs6wg'
+BOT_TOKEN = '6306869964:AAHOBsBqXnnLAoCn-edg47CuncGyJ0kqeLs'  # бот для теста администрации
 # BOT_TOKEN = '5495619130:AAF6kMuH9I0rNquGd3ma68Ld66XGAriWx2c'  # тест бот Никиты
